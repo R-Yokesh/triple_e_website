c17d83dcf3f4ce89d772b6ad67f8d8d6c1230de2
R-Yokesh <yokeshravichandran10@gmailcom>
2024-10-01 15:27:36 +0530

    Font Changes !

--- src/Components/CountSection/CountSection.jsx
+++ src/Components/CountSection/CountSection.jsx
@@ -4,9 +4,24 @@ function CountSection({ data }) {
     return (
         <section className='count-sec'>
             <h2 className='number'>{data.count}</h2>
-            <h4 className={data.clr}>{data.name}</h4>
+            <h4 className={data.clr}>
+                {/* Check if the name is 'Regions in KSA', then split it manually */}
+                {data.name === 'Regions in KSA' ? (
+                    <>
+                        Regions in <br /> KSA
+                    </>
+                ) : (
+                    // Default behavior for other names
+                    data.name.split(' ').map((part, index) => (
+                        <span key={index}>
+                            {part}
+                            {index !== data.name.split(' ').length - 1 && <br />}
+                        </span>
+                    ))
+                )}
+            </h4>
         </section>
-    )
+    );
 }
 
-export default CountSection
+export default CountSection;
